4d82dc616383fc17470c355e4f62ec5d41477d0b
MyoThuHtay <61018567+MyoThuHtay@users.noreply.github.com>
2023-01-15 21:23:54 +0630

    test

---
+++ server/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+
+async function auth  (req, res, next) {
+
+    try {
+        const token = req.header('x-auth-token');
+        if (!token) return res
+            .status(401)
+            .json({ msg: "Access Denied" });
+
+        const verify = jwt.verify(token, 'passwordKey');
+        if (!verify) return res
+            .status(401)
+            .json({ msg: 'Token Verification failed, Access Denied' });
+        req.user = verify.id;
+        req.token = token;
+        next();
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+module.exports = auth;
